f83084eff9a5c4c03ce5059797634d5409738bd4
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2023-10-19 23:07:40 +0600

    added binary search

--- hw7/main.cpp
+++ hw7/main.cpp
@@ -10,6 +10,14 @@ class VectorArray {
 private:
     vector<T> array;
 
+    int search(T element, int begin, int mid, int end) {
+        if (element == mid) {
+            return mid;
+        } else if (element < mid) { return search(element, begin, (begin + mid) / 2, mid); }
+        else if (element > mid) { return search(element, mid, (mid + end) / 2, end); }
+        else return -1;
+    }
+
 public:
     explicit VectorArray(vector<T> new_array) : array(std::move(new_array)) {};
 
@@ -45,8 +53,16 @@ public:
         std::sort(array.begin(), array.end());
     }
 
+    bool is_sorted() {
+        for (int i = 0; i < size() - 1; i++) {
+            if (array[i] > array[i + 1]) return false;
+        }
+        return true;
+    }
+
     int binary_search(int index, T element) {
-//    VectorArray copy = // TODO we need copy constructor here
+        if (!is_sorted()) throw logic_error("Array must be sorted before binary search");
+        return search(element, 0, size() / 2, size());
     }
 
     void set_element(int index, T element) {

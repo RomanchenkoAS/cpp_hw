750f2cd23b88da2faf4a0bb81a19ece6559a5d1e
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2023-10-19 23:23:32 +0600

    fixed search logic, added inline print method

--- hw7/main.cpp
+++ hw7/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <algorithm> // for sort
-#include <utility>
+#include <utility> // IDE added this idk what for
 #include <vector>    // for vector
 
 using namespace std;
@@ -10,12 +10,14 @@ class VectorArray {
 private:
     vector<T> array;
 
-    int search(T element, int begin, int mid, int end) {
-        if (element == mid) {
-            return mid;
-        } else if (element < mid) { return search(element, begin, (begin + mid) / 2, mid); }
-        else if (element > mid) { return search(element, mid, (mid + end) / 2, end); }
-        else return -1;
+    int search(T element, int begin, int end) {
+        if (begin <= end) {
+            int mid = begin + (end - begin) / 2;
+            if (array[mid] == element) return mid;
+            if (array[mid] > element) return search(element, begin, mid - 1);
+            return search(element, mid + 1, end);
+        }
+        return -1;
     }
 
 public:
@@ -29,8 +31,15 @@ public:
         }
     }
 
-    VectorArray &operator==(const VectorArray &other) {
-        if (array.size() != other.size()) {
+    void print_inline() {
+        for (auto element: array) {
+            cout << element << "\t";
+        }
+        cout << endl;
+    }
+
+    bool operator==(const VectorArray &other) {
+        if (this->get_size() != other.get_size()) {
             return false;
         }
         for (int i = 0; i < array.size(); i++) {
@@ -41,7 +50,7 @@ public:
         return true;
     }
 
-    int size() {
+    int get_size() const {
         return array.size();
     }
 
@@ -54,15 +63,15 @@ public:
     }
 
     bool is_sorted() {
-        for (int i = 0; i < size() - 1; i++) {
+        for (int i = 0; i < get_size() - 1; i++) {
             if (array[i] > array[i + 1]) return false;
         }
         return true;
     }
 
-    int binary_search(int index, T element) {
-        if (!is_sorted()) throw logic_error("Array must be sorted before binary search");
-        return search(element, 0, size() / 2, size());
+    int binary_search(T element) {
+        if (!is_sorted()) throw logic_error("Logic error: Array must be sorted before binary search.");
+        return search(element, 0, get_size() - 1);
     }
 
     void set_element(int index, T element) {
@@ -84,18 +93,42 @@ int main() {
     object.print();
 
     // Checking == operator
-    vector<int> arr1 = {10, 2, 3, 4, 5, 0, 1};
-    vector<int> arr2 = {10, 2, 3, 4, 5, 0, 1, 1};
+    cout << "Checking equality operator: " << endl;
+
+    VectorArray arr1(vector<int>{10, 2, 3, 4, 5, 0, 1});
+    VectorArray arr2(vector<int>{10, 2, 3, 4, 5, 0, 1, 1});
 
     bool equality1 = (arr1 == arr2);
     cout << "Equality 1 = " << equality1 << endl;
 
-    vector<int> arr3 = {10, 2, 3, 4, 5, 0, 1};
-    vector<int> arr4 = {10, 2, 3, 4, 5, 0, 1};
+    VectorArray arr3(vector<int>{10, 2, 3, 4, 5, 0, 1});
+    VectorArray arr4(vector<int>{10, 2, 3, 4, 5, 0, 1});
 
     bool equality2 = (arr3 == arr4);
     cout << "Equality 2 = " << equality2 << endl;
 
+    vector<int> arr5 = {10, 2, 3, 4, 5, 0, 1};
+    VectorArray object5(arr5);
+    cout << "\nBinary search:\nLook for 5 in array:" << endl;
+    object5.print_inline();
+    cout << "\nExpect an error:" << endl;
+    try {
+        int result = object5.binary_search(5);
+        cout << "Found 5 at position " << result << endl;
 
+    } catch (const std::exception &e) {
+        cout << e.what() << endl;
+    }
+    object5.sort();
+    cout << "Look for 5 in array:" << endl;
+    object5.print_inline();
+    cout << "\nExpect a value:" << endl;
+    try {
+        int result = object5.binary_search(5);
+        cout << "Found 5 at position " << result << endl;
+
+    } catch (const std::exception &e) {
+        std::cerr << "Caught exception: " << e.what() << std::endl;
+    }
     return 0;
 }

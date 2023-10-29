1e6b373e326bb722c9c02f869478051e09153371
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2023-10-29 15:57:07 +0600

    error handling

--- hw12/main.cpp
+++ hw12/main.cpp
@@ -8,14 +8,38 @@ class FilerBase {
 private:
     ifstream file;
 public:
+    FilerBase() : file() {};
+    ~FilerBase() {
+        if (is_open()) file.close();
+    }
+
+    bool is_open() { return (file.is_open()); }
+
     virtual void open_file(const string &filename) {
         file.open(filename);
+        if (!file) {
+            throw runtime_error("Failed to open " + filename);
+        }
     };
 
-    virtual void print() {};
+    virtual void print() {
+        char output = 0;
+        if (is_open()) {
+            while (file.get(output)) {
+                std::cout << output;
+            }
+        }
+    };
 };
 
 int main() {
 
+    FilerBase f1;
+    try {
+        f1.open_file("hw12/output/file.txt");
+        f1.print();
+    } catch(const exception& e) {
+        cout << e.what() << endl;
+    }
     return 0;
 }

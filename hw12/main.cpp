0568c83ec7ebcd4120f78e43d240ac0ea2548ff4
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2023-10-29 15:26:45 +0600

    base class

--- hw12/main.cpp
+++ hw12/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <string>
+#include <fstream>
 
 using namespace std;
+
+class FilerBase {
+private:
+    ifstream file;
+public:
+    virtual void open_file(const string &filename) {
+        file.open(filename);
+    };
+
+    virtual void print() {};
+};
 
 int main() {
 

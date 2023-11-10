e454a57c917da4cfdcb91e7c8850c7c0f6d152a4
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2023-11-11 01:44:07 +0600

    its working omg

--- hw16/main.cpp
+++ hw16/main.cpp
@@ -39,17 +39,21 @@ public:
     };
 
 //    You can copy and assign this object!
-    smart_shared_ptr(const smart_shared_ptr &other) : pointer(other.pointer), counter(other.counter + 1) {
-        return *this;
-    };
+    smart_shared_ptr(const smart_shared_ptr &other) : pointer(other.pointer), counter(other.counter + 1) {};
 
     smart_shared_ptr &operator=(const smart_shared_ptr &other) {
         if (this != &other) {
             pointer = other.pointer;
-            counter = ++other.counter;
+            other++;
+            counter = other.counter;
         }
         return *this;
     };
+
+    smart_shared_ptr &operator++() {
+        counter++;
+        return *this;
+    }
 
     T &operator*() {
         return *pointer;
@@ -57,6 +61,8 @@ public:
 };
 
 int main() {
+    std::cout << "This is testing unique ptr." << std::endl;
+
     {
         smart_ptr<int> p1(new int);
         *p1 = 5;
@@ -64,5 +70,15 @@ int main() {
         std::cout << "Ptr 1 = " << *p1 << std::endl;
     }
     std::cout << "This is outside local namespace." << std::endl;
+
+    std::cout << "\nThis is testing shared ptr." << std::endl;
+    {
+        smart_shared_ptr<int> p1(new int);
+        *p1 = 5;
+        smart_shared_ptr<int> p2(p1);
+        std::cout << "Inside local namespace:" << std::endl;
+        std::cout << "Ptr 1 = " << *p1 << std::endl;
+        std::cout << "Ptr 2 = " << *p2 << std::endl;
+    }
     return 0;
 }

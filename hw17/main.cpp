a53e323fa7bd6b939cb7aa0cfba9a5559c4af9f8
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2023-11-20 21:53:54 +0600

    basic

---
+++ hw17/main.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <fstream>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+class FilerBase {
+protected:
+    ifstream file;
+
+    virtual void print_char(char output) {
+        /* Display a single character into console */
+        cout << output;
+    };
+public:
+    FilerBase() : file() {};
+
+    virtual ~FilerBase() {
+        if (is_open()) file.close();
+    }
+
+    bool is_open() { return (file.is_open()); }
+
+    void open_file(const string &filename) {
+        file.open(filename);
+        if (!file) {
+            throw runtime_error("Failed to open " + filename);
+        }
+    };
+
+    void print() {
+        char output = 0;
+        try {
+            if (is_open()) {
+                while (file.get(output)) {
+                    print_char(output);
+                }
+            } else {
+                throw runtime_error("File is not open.");
+            }
+        } catch (const exception &e) {
+            cerr << "Error: " << e.what() << endl;
+        }
+    }
+};
+
+class Mapper : public FilerBase {
+private:
+    string text_body;
+    unordered_map<string, int> dictionary;
+public:
+    void print_char(char output) override {
+        cout << (int) output << " ";
+    };
+
+    void read_text() {
+        char output = 0;
+        try {
+            if (is_open()) {
+                while (file.get(output)) {
+                    text_body += output;
+                }
+            } else {
+                throw runtime_error("File is not open.");
+            }
+        } catch (const exception &e) {
+            cerr << "Error: " << e.what() << endl;
+        }
+    }
+
+    void compose_dictionary() {
+        string word;
+        for (char c: text_body) {
+            if (isalpha(c) || c == '\'') {
+                word += c;
+            } else {
+                transform(word.begin(), word.end(), tolower);
+                if (!word.empty()) {
+                    if (dictionary.count(word) == 0) {
+                        dictionary.insert({word, 1});
+                    } else {
+                        dictionary[word]++;
+                    }
+                }
+                word.clear();
+            }
+        }
+    }
+
+    void show_dictionary() {
+        for (auto it = dictionary.begin(); it != dictionary.end(); ++it) {
+            std::cout << it->first << ": " << it->second << std::endl;
+        }
+    }
+
+    string get_text_body() {
+        return text_body;
+    }
+};
+
+int main() {
+
+    Mapper mapper;
+//    SET CORRECT FILE PATH
+    mapper.open_file("/home/artur/dev/cpp_hw/hw17/textfile");
+    mapper.read_text();
+//    cout << mapper.get_text_body() << endl;
+    mapper.compose_dictionary();
+    mapper.show_dictionary();
+    return 0;
+}

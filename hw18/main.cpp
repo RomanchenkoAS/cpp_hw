2e4509b077d72da366ddf806cfb4f080d129f214
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2023-11-20 22:43:23 +0600

    done

--- hw18/main.cpp
+++ hw18/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 
 using namespace std;
-
 int main() {
 
-
+    auto isnum = [](char c) { return (bool)isdigit(c); };
+    cout << isnum('1') << endl;
+    cout << isnum('a') << endl;
 
     return 0;
 }
